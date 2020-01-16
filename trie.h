96d39bdcfa37b74b2a5e0226fb8a9b379a8453bd
maxmaxim6 <57861712+maxmaxim6@users.noreply.github.com>
2020-01-16 10:37:39 +0200

    Update trie.h

--- trie.h
+++ trie.h
@@ -1,6 +1,9 @@
 #ifndef _TRIE
 #define _TRIE
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 1000
 
 
 #define NUM_LETTERS 26
@@ -10,13 +13,14 @@ typedef struct node
     bool end_string;
     int count;
     struct node *next_char[NUM_LETTERS];
+    char ch;
 }node;
 
 node* newNode();
 void insert( node **head , char *str);
 void PrintAll(node **root,char *word ,int index);
 void PrintRev(node **root,char *word ,int index);
-bool cast(char *word);
+void cast(char **word);
 bool checkEmpty(node **root);
 void frees(node **root);
 
