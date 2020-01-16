a1d6a8d999a2e61b086c0117160e90343d261243
maxmaxim6 <57861712+maxmaxim6@users.noreply.github.com>
2020-01-16 10:38:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
-#include "trie.h"
 #include <string.h>
+#include "trie.h"
 #define SIZE 1000
+
 
 
 int main(int argc , char *argv[])
@@ -16,31 +18,30 @@ int main(int argc , char *argv[])
         int i=0,j=0;
         while(a[i]!='\0')
         {
-            if(a[i]!=' ')
+            if((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z'))
             {
                 b[j++]+=a[i];
             }
             else
             {
-                b[j]='\0';
-                if(cast(b))
-                {
-                    insert(&root,b);
-                }
+                b[j]='\0';    
+        
+                insert(&root,b);
                 memset(b,0,sizeof(b)); // reset the string
                 j=0;           
             }
             i++;   
         }
+        memset(b,0,sizeof(b));
     }
     if(argc!=1)
     {
-       PrintRev(&root,temp,0);
+        PrintRev(&root,temp,0);
     }
     else
     {
-       PrintAll(&root,temp,0);
+        PrintAll(&root,temp,0);
+        frees(&root);
     }
-    frees(&root);
     return 0;
 }
